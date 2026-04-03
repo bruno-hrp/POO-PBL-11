618ce05927e2617a8afa0e7dc5881a9999b16081
bruno-hrp <brunohrp06@gmail.com>
2026-04-03 15:47:22 -0300

    Update Fornecedor.java
    Molde adicionado

--- Fornecedor.java
+++ Fornecedor.java
@@ -1,3 +1,40 @@
-public class Fornecedor {
-    
+public class Fornecedor implements Validador, Calculavel {
+    //Atributos
+    private String cnpj;
+    private boolean dadosCompletos;
+
+    //Métodos
+    @Override
+    public boolean validar() {
+
+    }
+
+    @Override
+    public double calcular() {
+
+    }
+
+    //Construtor
+    public Fornecedor(String cnpj, boolean dadosCompletos) {
+        this.cnpj = cnpj;
+        this.dadosCompletos = dadosCompletos;
+    }
+
+    //Getters e setters
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public boolean isDadosCompletos() {
+        return dadosCompletos;
+    }
+
+    public void setDadosCompletos(boolean dadosCompletos) {
+        this.dadosCompletos = dadosCompletos;
+    }
+
 }
