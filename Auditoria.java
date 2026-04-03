987c9889e769143c3e5ac656f4e1902b9f4d70ce
bruno-hrp <brunohrp06@gmail.com>
2026-04-03 15:46:58 -0300

    Update Auditoria.java
    Molde adicionado

--- Auditoria.java
+++ Auditoria.java
@@ -1,3 +1,24 @@
-public class Auditoria {
-    
+public class Auditoria implements Auditavel {
+    //Atributos
+    private List<ContratoPublico> contratos;
+
+    //Métodos
+    @Override
+    public void auditar() {
+
+    }
+
+    //Construtor
+    public Auditoria(List<ContratoPublico> contratos) {
+        this.contratos = contratos;
+    }
+
+    //Getters e setters
+    public List<ContratoPublico> getContratos() {
+        return contratos;
+    }
+
+    public void setContratos(List<ContratoPublico> contratos) {
+        this.contratos = contratos;
+    }
 }
