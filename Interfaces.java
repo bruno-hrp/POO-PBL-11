c16f2af1234c5d4194d10522d0bbdde9634970dc
bruno-hrp <brunohrp06@gmail.com>
2026-04-03 14:35:29 -0300

    Update Interfaces.java

---
+++ Interfaces.java
@@ -0,0 +1,15 @@
+interface Validador {
+    boolean validar();
+}
+
+interface Calculavel {
+    double calcular();
+}
+
+interface Auditavel {
+    void auditar();
+}
+
+interface Relatavel {
+    void gerarRelatorio();
+}
