6ce138b24550b0b709314ad25c4ee8e41c3f8168
bruno-hrp <brunohrp06@gmail.com>
2026-04-03 15:47:31 -0300

    Update NotaFiscal.java
    molde adicionado

--- NotaFiscal.java
+++ NotaFiscal.java
@@ -1,3 +1,35 @@
-public class NotaFiscal {
-    
+public class NotaFiscal implements Validador {
+    //Atributos
+    private double valor;
+    private boolean consistente;
+
+    //Métodos
+    @Override
+    public boolean validar() {
+
+    }
+
+    //Construtor
+    public NotaFiscal(double valor, boolean consistente) {
+        this.valor = valor;
+        this.consistente = consistente;
+    }
+
+    //Getters e setters
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public boolean isConsistente() {
+        return consistente;
+    }
+
+    public void setConsistente(boolean consistente) {
+        this.consistente = consistente;
+    }
+
 }
