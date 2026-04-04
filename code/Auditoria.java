8f0caf0fac6236874e3ed3ecd04c2e1b29372dba
bruno-hrp <brunohrp06@gmail.com>
2026-04-04 19:57:47 -0300

    try-catch na auditoria

--- code/Auditoria.java
+++ code/Auditoria.java
@@ -1,3 +1,5 @@
+import java.util.List;
+
 public class Auditoria implements Auditavel {
     //Atributos
     private List<ContratoPublico> contratos;
@@ -5,7 +7,35 @@ public class Auditoria implements Auditavel {
     //Métodos
     @Override
     public void auditar() {
+        System.out.println("- Iniciando auditoria...");
+        System.out.println("Total de contratos a analisar: " + contratos.size());
 
+        for(ContratoPublico contrato : contratos) {
+            try {
+                if(contrato == null) { //Contrato não existente
+                    throw new NullPointerException("O contrato não existe na lista.");
+                }
+
+                System.out.println("\n - Analisando contrato: " + contrato.getDescricao() + ".");
+
+                boolean isValido = contrato.validar();
+                double percentual = contrato.calcular();
+
+                if(isValido && percentual != -1.0) {
+                    System.out.println("Auditoria concluída.");
+                } else {
+                    System.out.println("Auditoria apontou irregularidades neste contrato.");
+                }
+            } catch (NullPointerException e) {
+                System.out.println("Falha de acesso." + e.getMessage());  
+            } catch (Exception e) {
+                System.out.println("Falha inesperada." + e.getMessage());
+            } finally {
+                System.out.println("- Item analisado. Avançando...");
+            }
+        }
+
+        System.out.println("- Auditoria encerrada.");
     }
 
     //Construtor
