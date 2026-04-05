4ff7fec2a843d905e12194129229d3d049663247
Guilherme Otávio Leandro Soares <guilherme.leandro.soares049@gmail.com>
2026-04-05 15:45:40 -0300

    Update Main.java

--- code/Main.java
+++ code/Main.java
@@ -14,21 +14,27 @@ public class Main {
             ContratoPublico c2 = new ContratoPublico("Contrato 2", 0, 300, f1);
             ContratoPublico c3 = new ContratoPublico("Contrato 3", 1000, 1500, f2);
             ContratoPublico c4 = null;
+            ContratoPublico c5 = new ContratoPublico("Contrato 4", 500, 200, null);
             
 
             // Notas fiscais
             NotaFiscal nf1 = new NotaFiscal(500, true);
             NotaFiscal nf2 = new NotaFiscal(-100, true); // erro
             NotaFiscal nf3 = null; // erro proposital
+            NotaFiscal nf4 = new NotaFiscal(300, true);
+            NotaFiscal nf5 = new NotaFiscal(1500, false); // inconsistente
 
             c1.adicionarNotaFiscal(nf1);
             c1.adicionarNotaFiscal(nf2);
             c1.adicionarNotaFiscal(nf3);
+            c2.adicionarNotaFiscal(nf4);
+            c3.adicionarNotaFiscal(nf5);
 
             // Associação
             f1.adicionarContrato(c1);
             f1.adicionarContrato(c2);
             f2.adicionarContrato(c3);
+            
 
             // Lista
             List<ContratoPublico> contratos = new ArrayList<>();
@@ -36,6 +42,8 @@ public class Main {
             contratos.add(c2);
             contratos.add(c3);
             contratos.add(c4);
+            contratos.add(c5);
+
 
             // Auditoria
             Auditoria auditoria = new Auditoria(contratos);
