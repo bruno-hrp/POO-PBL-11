f3c3b60a503f34f8bc250e871b9f8a8647eb6588
Guilherme Otávio Leandro Soares <guilherme.leandro.soares049@gmail.com>
2026-04-05 15:38:27 -0300

    Update Main.java

---
+++ code/Main.java
@@ -0,0 +1,52 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+        try {
+            // Fornecedores
+            Fornecedor f1 = new Fornecedor("123", true, "Empresa Boa");
+            Fornecedor f2 = new Fornecedor(null, false, null);
+
+            // Contratos
+            ContratoPublico c1 = new ContratoPublico("Contrato 1", 1000, 500, f1);
+            ContratoPublico c2 = new ContratoPublico("Contrato 2", 0, 300, f1);
+            ContratoPublico c3 = new ContratoPublico("Contrato 3", 1000, 1500, f2);
+            ContratoPublico c4 = null;
+            
+
+            // Notas fiscais
+            NotaFiscal nf1 = new NotaFiscal(500, true);
+            NotaFiscal nf2 = new NotaFiscal(-100, true); // erro
+            NotaFiscal nf3 = null; // erro proposital
+
+            c1.adicionarNotaFiscal(nf1);
+            c1.adicionarNotaFiscal(nf2);
+            c1.adicionarNotaFiscal(nf3);
+
+            // Associação
+            f1.adicionarContrato(c1);
+            f1.adicionarContrato(c2);
+            f2.adicionarContrato(c3);
+
+            // Lista
+            List<ContratoPublico> contratos = new ArrayList<>();
+            contratos.add(c1);
+            contratos.add(c2);
+            contratos.add(c3);
+            contratos.add(c4);
+
+            // Auditoria
+            Auditoria auditoria = new Auditoria(contratos);
+
+            auditoria.auditar();
+
+        } catch (Exception e) {
+            System.out.println("Erro geral na main: " + e.getMessage());
+
+        } finally {
+            System.out.println("Sistema encerrado.");
+        }
+    }
+}
